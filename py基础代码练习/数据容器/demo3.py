21d62956f7ba9ea4d96a67fe128f4f682716b087
kittio0 <liujiahao565859@qq.com>
2026-09-10 17:11:18 +0800

    feat: 学习字符串str

---
+++ py基础代码练习/数据容器/demo3.py
@@ -0,0 +1,28 @@
+
+s="abcdefg-abab"
+
+print(s)
+print(s[2])
+print(s[0:5:1])
+print(s[-1:-6:-1])
+
+#find 查找子字符串，返回第一次出现的索引，没有返回-1
+index=s.find("c")
+print(index)
+
+#count 统计子串在字符串中出现的次数
+count=s.count("a")
+print(count)
+
+#upper 转大写
+s=s.upper()
+print(s)
+
+#lower 转小写
+s=s.lower()
+print(s)
+
+#replace 替换子串
+s=s.replace("a","x")
+print(s)
+
